41bcc680ce107cbac15eb045e2eb3b328d431977
sixc213 <ew846346@gmail.com>
2026-03-29 09:07:31 +0100

    Update check-key.js

--- api/check-key.js
+++ api/check-key.js
@@ -6,24 +6,16 @@ function generateKey() {
   return "VERITAS-" + Math.random().toString(36).substring(2,10).toUpperCase();
 }
 
+import { keys } from "./create-key";
+
 export default function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
+  const key = req.headers["x-api-key"];
+
+  const valid = keys.find(k => k.key === key);
+
+  if (!valid) {
+    return res.status(403).json({ valid:false });
   }
 
-  const { email } = req.body;
-
-  const newKey = generateKey();
-
-  keys.push({
-    email,
-    key: newKey,
-    plan: "PRO",
-    createdAt: Date.now()
-  });
-
-  res.status(200).json({
-    success: true,
-    key: newKey
-  });
+  res.status(200).json({ valid:true });
 }
